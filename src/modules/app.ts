e64cd3766be748d625b86b605f3e9f6461a90ad5
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2023-05-12 14:18:46 +0200

    Update app.ts

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -444,7 +444,7 @@ async function redirectToLogin() {
   await logoutAndUpdateStatus();
 
   //omdirigerar användaren till hemsidan (inloggningssidan)
-    window.location.href = "https://elli2022.github.io/slutprojekt-last-master/index.html";
+    window.location.href = "https://elli2022.github.io/slutprojekt-last-master-master-main/";
   
 }
 
